2c1bbfcc3c4bea7e42c6396c08e49678b64c2f5e
wangyangjun <wangyangjun@huice.com>
2020-09-08 14:27:58 +0800

    自定义导航栏 隐藏

---
+++ 小程序/测试滑动 Demo/pages/index/indexList.vue
@@ -0,0 +1,47 @@
+<template>
+	
+	<view>
+		<block v-for="(item, index) in list">
+			<view class="item" @click="itemClick(index)">{{item.title}}</view>
+		</block>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list:[
+					{title: "仿iOS section header效果",path: "/pages/index/index"},
+					{title: "隐藏导航栏",path: "/pages/index/navigateHidden"},
+				]
+			}
+		},
+		onLoad() {
+	
+		},
+	
+		onReady() {
+			uni.hideTabBar({
+				
+			})
+		},
+	
+		methods: {
+			itemClick(i) {
+				uni.navigateTo({
+					url: this.list[i].path
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.item {
+		margin: 20rpx;
+		background-color: #C8C7CC;
+		padding: 20rpx 40rpx;
+		color: #000000;
+	}
+</style>
---
+++ 小程序/测试滑动 Demo/pages/index/navigateHidden.vue
@@ -0,0 +1,102 @@
+<template>
+	<view>
+		<view class="navigate-box" :style="{'padding-top': statusBarHeight + 'px',opacity: naviBarOpacity}">
+			<view class="navigate-content f-a-c">
+				<view class="left f-c" @click="back">back</view>
+				<view class="title f-c">自定义导航栏</view>
+				<view class="left f-c"></view>
+			</view>
+		</view>
+		
+		<view class="page-content"></view>
+		<view class="page-content"></view>
+		<view class="page-content"></view>
+		<view class="page-content"></view>
+		<view class="page-content"></view>
+		
+		<view></view>
+	</view>
+	
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				statusBarHeight: 0, 	//状态栏高度
+				naviBarHeight: 44,		// 导航栏高度
+				naviBarOpacity: 0,
+			}
+		},
+		onLoad() {
+			const res = uni.getSystemInfoSync();
+			console.log("设别:", res);
+			this.statusBarHeight = res.statusBarHeight;
+		},
+	
+		onReady() {
+			
+		},
+		
+		onPageScroll(e) {
+			// min max 表示 透明的开始和结束范围 根据比例显示
+			var min = 40;
+			var max = 120;
+			var opacity = 0;
+			if (e.scrollTop > max) {
+				opacity = 1;
+			}else if (e.scrollTop > min) {
+				opacity = (e.scrollTop - min) / (max - min);
+			}
+			
+			this.naviBarOpacity = opacity;
+		},
+	
+		methods: {
+			back() {
+				uni.navigateBack({});
+			}
+		}
+	}
+</script>
+
+<style>
+	.navigate-box {
+		position: fixed;
+		top: 0;
+		width: 100%;
+		overflow: hidden;
+		background-color: #1D69A9;
+	}
+	
+	.navigate-content {
+		text-align: center;
+		height: 44px;
+	}
+	
+	.f-a-c {
+		display: flex;
+		align-items: center;
+	}
+	
+	.f-c {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	
+	.navigate-content .left {
+		height: 100%;
+		width: 120rpx;
+	}
+	.navigate-content .title {
+		flex: 1;
+		height: 100%;
+	}
+	
+	.page-content {
+		margin-bottom: 50rpx;
+		height: 400rpx;
+		background-color: #F0AD4E;
+	}
+</style>
