f1626ea4a64b970fc752350ce64b4a39c9052cae
wangyangjun <wangyangjun@huice.com>
2020-09-26 21:02:18 +0800

    将同步遍历查询子目录 改为异步

--- js脚本 自定义条件编译模式/precompile/build.js
+++ js脚本 自定义条件编译模式/precompile/build.js
@@ -6,62 +6,73 @@ var buildFile = require("./platform.js");
 var arguments = process.argv.splice(2);
 buildFile.setPlatform(arguments[0]);
 
-// 记录当前目录是否不需要编译 直接copy
-var currentNoBuildPath = "";
-
 var copy = function(src, dst) {
-	
-	if (findCopyPath(src)) {
-		currentNoBuildPath = src;
-	}
 	
 	let paths = fs.readdirSync(src); //同步读取当前目录
 	paths.forEach(function(path) {
 		var _src = src + '/' + path;
 		var _dst = dst + '/' + path;
 		
-		// 不能使用异步  因为需要对文件夹遍历设置是否编译 否则回调会导致冲突
-		// fs.stat(_src, function(err, stats) { //stats 该对象 包含文件属性
-		// 	if (err) throw err;
-		let stats = fs.statSync(_src);//stats 该对象 包含文件属性
-		if (stats.isFile()) { //如果是个文件则拷贝
-			
-			// copy文件 不进行条件编译
-			if (currentNoBuildPath) {
-				let readable = fs.createReadStream(_src); //创建读取流
-				let writable = fs.createWriteStream(_dst); //创建写入流
-				readable.pipe(writable);
-			}else {
-				buildFile.write(_src, _dst);
-			}
-		} else if (stats.isDirectory()) { //是目录则 递归
-			checkDirectory(_src, _dst, copy);
-		}
+		// 同步的话则不需要 遍历目录 但是耗时 而且不方便支持差量编译
+		// let stats = fs.statSync(_src);//stats 该对象 包含文件属性
+		// 改为异步 目前在单独写的方法 在写文件的时候 直接判断是否是子目录， 可以提升速度
+		fs.stat(_src, function(err, stats) { //stats 该对象 包含文件属性
+			if (err) throw err;
+			writeFileOfStats(_src, _dst, stats);
+		})
 	});
-	
-	if (src == currentNoBuildPath) {
-		currentNoBuildPath = "";
-	}
 }
 var checkDirectory = function(src, dst, callback) {
 	fs.access(dst, fs.constants.F_OK, (err) => {
 		if (!err) {
-			deleteFile(dst);
+			deletePath(dst);
 		}
 		fs.mkdirSync(dst);
 		callback(src, dst);
 	});
 };
 
+function writeFileOfStats(_src, _dst, stats) {
+	if (stats.isFile()) {//如果是个文件则拷贝
+		writeFile(_src, _dst);
+	}else if (stats.isDirectory()) { //是目录则 递归
+		checkDirectory(_src, _dst, copy);
+	}
+}
+function writeFile(_src, _dst) {
+	// copy文件 不进行条件编译
+	if (findSubPathAll(config.copyPaths, _src)) {
+		let readable = fs.createReadStream(_src); //创建读取流
+		let writable = fs.createWriteStream(_dst); //创建写入流
+		readable.pipe(writable);
+	}else {
+		buildFile.write(_src, _dst);
+	}
+}
+
+
+var config = require('./platformConfig.js');
+const SOURCES_DIRECTORY = '../' + config.sourceCodePath; //源目录
+const TARGET_DIRECTORY = '../' + config.sourceTargetPath; //目标目录
+checkDirectory(SOURCES_DIRECTORY, TARGET_DIRECTORY, copy);
+
+
+
 // 删除指定目录 由于当前 只能递归删除 rmdirSync 只能删除空的文件
-function deleteFile(path) {
+function deletePath(path, file_bool) {
+	
+	if (file_bool) {
+		fs.unlinkSync(path);
+		return;
+	}
+	
     var files = [];
     if( fs.existsSync(path) ) {
         files = fs.readdirSync(path);
         files.forEach(function(file,index){
             var curPath = path + "/" + file;
             if(fs.statSync(curPath).isDirectory()) {
-                deleteFile(curPath);
+                deletePath(curPath);
             } else {
                 fs.unlinkSync(curPath);
             }
@@ -70,18 +81,40 @@ function deleteFile(path) {
     }
 };
 
-// 查询不编译的文件 查找是否是相应的目录文件
-function findCopyPath(path) {
-	let length = config.copyPaths.length;
+
+// 判断是不是所有子目录
+function findSubPathAll(paths,subPath) {
+	let length = paths.length;
 	for (var i = 0; i < length; i++) {
-		if (path == config.copyPaths[i]) {
+		if (findSubPath(paths[i], subPath)) {
 			return true;
 		}
 	}
 	return false;
 }
 
-var config = require('./platformConfig.js');
-const SOURCES_DIRECTORY = '../' + config.sourceCodePath; //源目录
-const TARGET_DIRECTORY = '../' + config.sourceTargetPath; //目标目录
-checkDirectory(SOURCES_DIRECTORY, TARGET_DIRECTORY, copy);
+// 判断是不是目录 
+// 弊端是对文件名含有“/”符号的,前缀与当前目录匹配的会判断错误，因此需要文件格式严禁或者后期优化
+function findSubPath(path, subPath) {
+	if (!path || !subPath) {
+		return false;
+	}
+	if (path == subPath) {
+		return true;
+	}
+	if (subPath.length > path.length && subPath.indexOf(path) == 0) {
+		var start = path.length;
+		let string = subPath.substring(start, start + 1);
+		if (string == "/") {
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+
+function sourcePathToTarget(path) {
+	var string = path.substring(SOURCES_DIRECTORY.length, path.length);
+	return TARGET_DIRECTORY + string;
+}
