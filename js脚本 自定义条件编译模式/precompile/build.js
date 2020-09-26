a6ddd6b8a1c8003b0828ac1bf9a2e54eccf9d0dc
wangyangjun <wangyangjun@huice.com>
2020-09-26 22:06:25 +0800

    重排代码格式

--- js脚本 自定义条件编译模式/precompile/build.js
+++ js脚本 自定义条件编译模式/precompile/build.js
@@ -1,4 +1,3 @@
-
 //  遍历文件目录
 var fs = require('fs');
 
@@ -7,12 +6,12 @@ var arguments = process.argv.splice(2);
 buildFile.setPlatform(arguments[0]);
 
 var copy = function(src, dst) {
-	
+
 	let paths = fs.readdirSync(src); //同步读取当前目录
 	paths.forEach(function(path) {
 		var _src = src + '/' + path;
 		var _dst = dst + '/' + path;
-		
+
 		// 同步的话则不需要 遍历目录 但是耗时 而且不方便支持差量编译
 		// let stats = fs.statSync(_src);//stats 该对象 包含文件属性
 		// 改为异步 目前在单独写的方法 在写文件的时候 直接判断是否是子目录， 可以提升速度
@@ -33,19 +32,20 @@ var checkDirectory = function(src, dst, callback) {
 };
 
 function writeFileOfStats(_src, _dst, stats) {
-	if (stats.isFile()) {//如果是个文件则拷贝
+	if (stats.isFile()) { //如果是个文件则拷贝
 		writeFile(_src, _dst);
-	}else if (stats.isDirectory()) { //是目录则 递归
+	} else if (stats.isDirectory()) { //是目录则 递归
 		checkDirectory(_src, _dst, copy);
 	}
 }
+
 function writeFile(_src, _dst) {
 	// copy文件 不进行条件编译
 	if (findSubPathAll(config.copyPaths, _src)) {
 		let readable = fs.createReadStream(_src); //创建读取流
 		let writable = fs.createWriteStream(_dst); //创建写入流
 		readable.pipe(writable);
-	}else {
+	} else {
 		buildFile.write(_src, _dst);
 	}
 }
@@ -58,60 +58,60 @@ checkDirectory(SOURCES_DIRECTORY, TARGET_DIRECTORY, copy);
 
 
 var watch = require('watch')
-watch.createMonitor(SOURCES_DIRECTORY, function (monitor) {
-    
-	monitor.on("created", function (f, stat) {
+watch.createMonitor(SOURCES_DIRECTORY, function(monitor) {
+
+	monitor.on("created", function(f, stat) {
 		writeFileOfStats(f, sourcePathToTarget(f), stat);
-    })
-	
+	})
+
 	// 只有文件修改 才会涉及到改变 如果是文件夹修改名称 则会走删除-再走创建
-    monitor.on("changed", function (f, curr, prev) {
-	  writeFile(f, sourcePathToTarget(f));
-    })
-	
-    monitor.on("removed", function (f, stat) {
-	  var dst = sourcePathToTarget(f);
-	  fs.stat(dst, function(err, stats){
-		  if (err) {
-			  return;
-		  }
-		  if (stats.isFile()) {
-		  		  deletePath(dst, true);
-		  }else if(stats.isDirectory()){
-		  		  deletePath(dst, false);
-		  }
-	  })
-    })
-    // monitor.stop(); // Stop watching
+	monitor.on("changed", function(f, curr, prev) {
+		writeFile(f, sourcePathToTarget(f));
+	})
+
+	monitor.on("removed", function(f, stat) {
+		var dst = sourcePathToTarget(f);
+		fs.stat(dst, function(err, stats) {
+			if (err) {
+				return;
+			}
+			if (stats.isFile()) {
+				deletePath(dst, true);
+			} else if (stats.isDirectory()) {
+				deletePath(dst, false);
+			}
+		})
+	})
+	// monitor.stop(); // Stop watching
 })
 
 // 删除指定目录 由于当前 只能递归删除 rmdirSync 只能删除空的文件
 function deletePath(path, file_bool) {
-	
-    var files = [];
-    if( fs.existsSync(path) ) {
-		
+
+	var files = [];
+	if (fs.existsSync(path)) {
+
 		if (file_bool) {
 			fs.unlinkSync(path);
 			return;
 		}
-		
-        files = fs.readdirSync(path);
-        files.forEach(function(file,index){
-            var curPath = path + "/" + file;
-            if(fs.statSync(curPath).isDirectory()) {
-                deletePath(curPath);
-            } else {
-                fs.unlinkSync(curPath);
-            }
-        });
-        fs.rmdirSync(path);
-    }
+
+		files = fs.readdirSync(path);
+		files.forEach(function(file, index) {
+			var curPath = path + "/" + file;
+			if (fs.statSync(curPath).isDirectory()) {
+				deletePath(curPath);
+			} else {
+				fs.unlinkSync(curPath);
+			}
+		});
+		fs.rmdirSync(path);
+	}
 };
 
 
 // 判断是不是所有子目录
-function findSubPathAll(paths,subPath) {
+function findSubPathAll(paths, subPath) {
 	let length = paths.length;
 	for (var i = 0; i < length; i++) {
 		if (findSubPath(paths[i], subPath)) {
@@ -137,7 +137,6 @@ function findSubPath(path, subPath) {
 			return true;
 		}
 	}
-	
 	return false;
 }
 
--- js脚本 自定义条件编译模式/precompile/platform.js
+++ js脚本 自定义条件编译模式/precompile/platform.js
@@ -6,24 +6,24 @@ var fs = require('fs');
 var readline = require('readline');
 
 var write = function(strInputFileName, strOutputFileName) {
-	
+
 	var fRead = fs.createReadStream(strInputFileName);
 	var fWrite = fs.createWriteStream(strOutputFileName);
-	
+
 	// fRead.on('end', () => {
 	// 	console.log('end');
 	// });
-	
+
 	var objReadline = readline.createInterface({
 		input: fRead,
 		output: fWrite,
 		terminal: false
 	});
-	
-	var type = "";			// 记录非当前平台条件编译开始的类型
-	var subTypeCount = 0; 	// 嵌套内部条件编译开始的计数
+
+	var type = ""; // 记录非当前平台条件编译开始的类型
+	var subTypeCount = 0; // 嵌套内部条件编译开始的计数
 	objReadline.on('line', (item) => {
-		
+
 		if (type) {
 			// 计算是否还有嵌套 其他的 if条件编译
 			if (condStart(item, "", type)) {
@@ -43,16 +43,16 @@ var write = function(strInputFileName, strOutputFileName) {
 		if (type) {
 			return;
 		}
-		
+
 		// 删除条件编译的行
 		if (conditionalLine(item)) {
 			return;
 		}
-		
+
 		fWrite.write(item + "\n");
 	});
-	
-	
+
+
 	// objReadline.on('close', () => {
 	// 	console.log('readline close...');
 	// });
@@ -74,7 +74,7 @@ function conditionalLine(string) {
 }
 
 // 遍历所有类型 html js的条件编译
-function conditionalStart(string,platformName){
+function conditionalStart(string, platformName) {
 	let type_list = allType();
 	for (var i = 0; i < type_list.length; i++) {
 		let type = type_list[i];
@@ -85,7 +85,7 @@ function conditionalStart(string,platformName){
 	return "";
 }
 //  判断结束: <!--#endif-->
-function conditionalEnd(string, type){
+function conditionalEnd(string, type) {
 	var regexp = new RegExp(condReg(type, 2));
 	return regexp.test(string);
 }
@@ -93,42 +93,42 @@ function conditionalEnd(string, type){
 // 条件编译 是否是平台 <!-- #ifdef  platform -->
 // paltform 为空的时候 默认认为找不到对应的平台 不写入
 // 返回true 表示不符合当前平台 不写入开始
-function condStart(string,platformName,type){
-	
+function condStart(string, platformName, type) {
+
 	var flag_if = 0; // 1表示 当前平台 2表示 不是当前平台
-	var regexp = new RegExp(condReg(type, 0));	// 是当前平台的
+	var regexp = new RegExp(condReg(type, 0)); // 是当前平台的
 	var regexp_n = new RegExp(condReg(type, 1)); // 非当前平台的
-	
+
 	if (regexp.test(string)) {
 		flag_if = 1;
-	}else if (regexp_n.test(string)) {
+	} else if (regexp_n.test(string)) {
 		flag_if = 2;
 	}
-	
+
 	if (flag_if != 0) {
-		
+
 		if (!platformName) {
 			return true;
 		}
-		
-		let index_flag = flag_if == 1 ? "#ifdef " : "#ifndef ";// “#ifdef ”的长度 还有空格 因为其实位置会从最后一个空格开始 包含 
+
+		let index_flag = flag_if == 1 ? "#ifdef " : "#ifndef "; // “#ifdef ”的长度 还有空格 因为其实位置会从最后一个空格开始 包含 
 		var start = string.indexOf(index_flag) + index_flag.length;
-		
+
 		var end_index = condReg(type, 3);
 		var end = string.length;
 		if (end_index) {
 			end = string.indexOf(end_index);
 		}
 		var res = string.substring(start, end);
-		
-		res = res.replace(/\s*/g,"");
+
+		res = res.replace(/\s*/g, "");
 		var list = res.split("||");
-		
+
 		// 如果没有相应的平台 则开始不写入
 		if (flag_if == 1 && !matchPlatform(list, platformName)) {
 			return true;
 		};
-		
+
 		// 相反的 #ifndef 如果有相应平台  则不写入
 		if (flag_if == 2 && list.indexOf(platformName) >= 0) {
 			return true;
@@ -138,18 +138,18 @@ function condStart(string,platformName,type){
 }
 
 // 判断当前平台是否符合 目前只能符合 平台的时候调用  #ifndef 不能用 负责其他的条件会导致无效的
-function matchPlatform (list, platform) {
+function matchPlatform(list, platform) {
 	var arr = [platform];
 	if (config[platform] && config[platform].coexist) {
-		arr = arr.concat(config[platform].coexist); 
+		arr = arr.concat(config[platform].coexist);
 	}
-	
+
 	for (var i = 0; i < arr.length; i++) {
 		if (list.indexOf(arr[i]) >= 0) {
 			return true;
 		}
 	}
-	
+
 	return false;
 }
 
@@ -161,28 +161,28 @@ function allType() {
 // type : html , json
 // keysIndex : 0 开始平台; 1开始非平台; 2 结束; 3结束标记
 function condReg(type, keysIndex) {
-	
+
 	let obj = {
 		html: {
-			reg_s: 		"^(\\s*<!--\\s*#ifdef\\s+)[\\s\\S]+\\s*-->",
-			reg_s_n: 	"^(\\s*<!--\\s*#ifndef\\s+)[\\s\\S]+\\s*-->",// 取反
-			reg_e: 		"^(\\s*<!--\\s*#endif)\\s*-->",
-			index_end: 	"-->"
+			reg_s: "^(\\s*<!--\\s*#ifdef\\s+)[\\s\\S]+\\s*-->",
+			reg_s_n: "^(\\s*<!--\\s*#ifndef\\s+)[\\s\\S]+\\s*-->", // 取反
+			reg_e: "^(\\s*<!--\\s*#endif)\\s*-->",
+			index_end: "-->"
 		},
 		js: {
-			reg_s: 		"^(\\s*//\\s*#ifdef\\s+)[\\s\\S]+\\s*",
-			reg_s_n: 	"^(\\s*//\\s*#ifndef\\s+)[\\s\\S]+\\s*",
-			reg_e: 		"^(\\s*//\\s*#endif)\\s*",
-			index_end: 	""
+			reg_s: "^(\\s*//\\s*#ifdef\\s+)[\\s\\S]+\\s*",
+			reg_s_n: "^(\\s*//\\s*#ifndef\\s+)[\\s\\S]+\\s*",
+			reg_e: "^(\\s*//\\s*#endif)\\s*",
+			index_end: ""
 		},
 		css: {
-			reg_s: 		"^(\\s*/[*]\\s*#ifdef\\s+)[\\s\\S]+\\s*[*]/",
-			reg_s_n: 	"^(\\s*/[*]\\s*#ifndef\\s+)[\\s\\S]+\\s*[*]/",
-			reg_e: 		"^(\\s*/[*]\\s*#endif)\\s*[*]/",
-			index_end: 	"*/"
+			reg_s: "^(\\s*/[*]\\s*#ifdef\\s+)[\\s\\S]+\\s*[*]/",
+			reg_s_n: "^(\\s*/[*]\\s*#ifndef\\s+)[\\s\\S]+\\s*[*]/",
+			reg_e: "^(\\s*/[*]\\s*#endif)\\s*[*]/",
+			index_end: "*/"
 		},
 	}
-	
+
 	var key = ["reg_s", "reg_s_n", "reg_e", "index_end"][keysIndex];
 	return obj[type][key];
 }
