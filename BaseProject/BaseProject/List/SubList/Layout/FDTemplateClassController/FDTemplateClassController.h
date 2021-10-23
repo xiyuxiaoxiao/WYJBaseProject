b22c5b07e100248f0abe84dd6b051d1a8232506c
wangyangjun <wangyangjun@huice.com>
2021-10-23 21:30:09 +0800

    修改错误

--- BaseProject/BaseProject/List/SubList/Layout/FDTemplateClassController/FDTemplateClassController.h
+++ BaseProject/BaseProject/List/SubList/Layout/FDTemplateClassController/FDTemplateClassController.h
@@ -6,11 +6,11 @@
 //  Copyright © 2019年 WYJ. All rights reserved.
 //
 
-#import "ViewController.h"
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface FDTemplateClassController : ViewController
+@interface FDTemplateClassController : UIViewController
 
 @end
 
